84558841dc9bf670c848d39a1df4ea829e2722c2
Pasindu Madushan <pasindu24hh@gmail.com>
2026-07-10 13:34:36 +0530

    Changes

--- App.tsx
+++ App.tsx
@@ -4,6 +4,8 @@ import { AuthProvider } from "./src/context/AuthContext";
 
 import AppNavigator from "./src/navigation/AppNavigator";
 
+import "./global.css";
+
 
 export default function App() {
 
